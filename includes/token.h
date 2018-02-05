8c2beb582a6738df22b27ea52c3d62627a26726f
Vincent BASTIONI <v.bastioni@gmail.com>
2018-02-05 18:36:52 +0100

    Added "real"

--- includes/token.h
+++ includes/token.h
@@ -6,12 +6,30 @@
 /*   By: lportay <lportay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/25 12:30:05 by lportay           #+#    #+#             */
-/*   Updated: 2018/01/29 18:24:31 by lportay          ###   ########.fr       */
+/*   Updated: 2018/02/05 17:22:09 by vbastion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKEN_H
 # define TOKEN_H
+
+
+# define R_LESS (LESS | DLESS | TLESS | LESSAND)
+# define R_GREAT (GREAT | DGREAT | GREATAND)
+# define R_AND (ANDGREAT | ANDDGREAT)
+
+# define REDIR (R_LESS | R_GREAT | R_AND)
+
+# define IS_LESS_REDIR(n) (n & (LESS | DLESS | TLESS | LESSAND))
+# define IS_GREAT_REDIR(n) (n & (GREAT | DGREAT | GREATAND))
+# define IS_AND_REDIR(n) (n & (ANDGREAT | ANDDGREAT))
+# define ISREDIR(n) (IS_LESS_REDIR(n) | IS_GREAT_REDIR(n) | IS_AND_REDIR(n))
+
+//c'est pas trop a la norme cette macro, a changer apres
+# define IS_MAX_OPE(n) (n & (OR_IF | AND_IF | DGREAT | LESSAND | GREATAND | ANDDGREAT | TLESS | DOLLAR | BANG | NEWLINE | SEMICOL))
+
+# define IS_NOT_MAX_OPE(n) (n & (OR | AND | LESS | GREAT | DLESS | ANDGREAT))
+# define IS_WORD_IO_NUMBER(n) (n & (WORD | ASSIGNMENT_WORD | IO_NUMBER))
 
 enum	e_toktype
 {
@@ -35,11 +53,12 @@ enum	e_toktype
 		GREATAND = 1 << 14,				// >&
 		TLESS = 1 << 15,				// <<<
 		ANDGREAT = 1 << 16,				// &>
+		ANDDGREAT = 1 << 17,			// &>>
 
-		DOLLAR = 1 << 17,				// $
-		BANG = 1 << 18,					// !
+		DOLLAR = 1 << 18,				// $
+		BANG = 1 << 19,					// !
 
-		COMMENT = 1 << 19,				// # *ANY_TOKEN NEWLINE
+		COMMENT = 1 << 20,				// # *ANY_TOKEN NEWLINE
 		PARAM_EXP = DOLLAR | 1 << 0,	// ${} | $VAR
 		CMD_SUB = DOLLAR | 1 << 2,		// $() |	bquotes+grouping --> 42SH
 		ARI_EXP = DOLLAR | 1 << 3,		// $[] | $((MATHS))
@@ -60,11 +79,23 @@ typedef struct		s_redir
 	struct s_token	*next;
 	enum e_toktype	type;
 
-	int				left_op;
-	int				fd_right_op;
-	char			*s_right_op;
+	int				lhs;
+	int				fd_rhs;
+	char			*s_rhs;
 	bool			dash;
 }					t_redir;
+
+typedef struct		s_heredoc
+{
+	t_dlist 		*first_letter;
+	t_dlist 		*last_letter;
+	struct s_token	*next;
+	enum e_toktype	type;
+
+	int				lhs;
+	char			*rhs;
+	t_line			hdoc;
+}					t_heredoc;
 
 char	*token_str(t_token *tok);
 void	delete_toklist(t_token **toklist);
