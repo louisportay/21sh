0d7e5ffb6ca1caa9309d9d03f41a5f80766e9290
Vincent BASTIONI <v.bastioni@gmail.com>
2018-02-06 17:05:12 +0100

    Ok, let's do it cleanly...

--- lib21sh/ft_stdio/stdio.mk
+++
@@ -1,6 +0,0 @@
-ITEMS+=\
-ft_putstr_fd.o\
-get_next_line.o
-
-SRC_D+=ft_stdio
-HEADERS+=ft_stdio.h
--- lib21sh/stack/stack.mk
+++
@@ -1,9 +0,0 @@
-ITEMS+=\
-stack_create.o\
-stack_del.o\
-stack_pop.o\
-stack_print.o\
-stack_push.o
-
-SRC_D+=stack
-HEADERS+=stack.h
