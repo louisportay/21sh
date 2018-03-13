5176c9b4760537372913692309ba38d760cae3df
Vincent BASTIONI <v.bastioni@gmail.com>
2018-03-13 14:50:53 +0100

    Kill done - Need to fix handling of background launched jobs and signaled jobs notification

--- srcs/builtins/builtin_kill.c
+++ srcs/builtins/builtin_kill.c
@@ -6,15 +6,48 @@
 /*   By: vbastion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/10 14:35:52 by vbastion          #+#    #+#             */
-/*   Updated: 2018/03/10 16:10:14 by vbastion         ###   ########.fr       */
+/*   Updated: 2018/03/13 14:46:14 by vbastion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_21sh.h"
 
+static void			lmultiargs(t_proc *p, t_ctx *ctx)
+{
+	int				i;
+	t_job			*j;
+//	t_list			*l;
+	t_list			*curr;
+//	char			*str;
+
+	i = 0;
+	curr = NULL;
+	while (p->argv[i] != NULL)
+	{
+		if ((j = jc_jobspec(p->argv[i], ctx)) != NULL)
+			kill(-j->pgid, SIGTERM);
+		else
+			;
+		i++;
+	}
+}
+
 int					ft_kill(t_proc *p, t_ctx *ctx)
 {
-	(void)ctx;
-	p->type = BU_STR;
+	char			*str;
+
+	p->type = BUILTIN;
+	if (p->argv[1] == NULL)
+	{
+		if (ctx->bgs == NULL)
+		{
+			asprintf(&str, BU_JOB_ERR, "kill", BU_JOB_NO);
+			p->data.out = list_create(str);
+		}
+		else
+			kill(-(((t_job *)ctx->bgs->content)->pgid), SIGTERM);
+	}
+	else
+		lmultiargs(p, ctx);
 	return (0);
 }
