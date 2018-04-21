592667e5f19b302879ff0fd25c295d39beab38b9
Vincent BASTIONI <vbastion@e3r3p12.42.fr>
2018-04-21 13:13:57 +0200

    Norme of expand

--- srcs/expand/expand.c
+++ srcs/expand/expand.c
@@ -6,7 +6,7 @@
 /*   By: vbastion <vbastion@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 19:18:58 by vbastion          #+#    #+#             */
-/*   Updated: 2018/04/20 18:16:42 by vbastion         ###   ########.fr       */
+/*   Updated: 2018/04/21 12:48:42 by vbastion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,13 @@ int					err(char *str)
 	return (0);
 }
 
-static void			lcleanup(int ret, int *exp, char **tmp, char **swp)
+static void			lcleanup(int ret, char **tmp, char **swp)
 {
 	if (ret > 0)
 	{
 		ft_strdel(tmp);
 		*tmp = *swp;
 		*swp = NULL;
-		*exp |= 1;
 	}
 }
 
@@ -50,23 +49,20 @@ int					expand(char *str, t_ctx *ctx, t_list **lst)
 	char			*tmp;
 	char			*swp;
 	int				ret;
-	int				exp;
 
 	if (ft_strcmp(str, "") == 0)
 		return (0);
-	exp = 0;
 	tmp = NULL;
 	ret = expand_tilde(str, &tmp, ctx);
 	tmp = (ret == 0) ? ft_strdup(str) : tmp;
-	exp |= (ret != 0);
 	ret = expand_braces(tmp, &swp);
-	lcleanup(ret, &exp, &tmp, &swp);
+	lcleanup(ret, &tmp, &swp);
 	if (ret == -1)
 		return (ret_err(&tmp, -3));
 	ret = expand_param(tmp, &swp, ctx);
 	if (ret == -1)
 		return (ret_err(&tmp, -3));
-	lcleanup(ret, &exp, &tmp, &swp);
+	lcleanup(ret, &tmp, &swp);
 	if ((ret = expand_split(tmp, lst)) == -1)
 		return (ret_err(&tmp, -1));
 	ft_strdel(&tmp);
